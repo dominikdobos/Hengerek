dfae692749f5d36a3c850f9b274ab7373ecce877
dominikdobos <dominik.dobos33@gmail.com>
2024-11-11 09:26:58 +0100

    hengerek oroklodese

---
+++ src/main/java/hu/szamalk/modell/LyukasHenger.java
@@ -0,0 +1,25 @@
+package hu.szamalk.modell;
+
+public class LyukasHenger extends TomorHenger {
+    private double falvastagsag;
+
+    public LyukasHenger(double sugar, double magassag, double fajsuly, double falvastagsag) {
+        super(sugar, magassag, fajsuly);
+        this.falvastagsag = falvastagsag;
+    }
+
+    public LyukasHenger(double sugar, double magassag, double falvastagsag) {
+        this(sugar, magassag, 1, falvastagsag);
+    }
+
+    public double getFalvastagsag() {
+        return falvastagsag;
+    }
+
+    @Override
+    public String toString() {
+        return "LyukasHenger{" +
+                "falvastagsag=" + falvastagsag +
+                '}';
+    }
+}
